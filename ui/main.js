db9c18e087b71bd0f2b3a44121884532523f0a7d
Anurag Bhattacharya <anurag.pro.01@gmail.com>
2017-08-17 18:11:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+
+//Code for moving the image on being clicked
+
+var image = document.getElementById("madi");
+
+var marginLeft = 0; 
+function moveRight () {
+    marginLeft = marginLeft + 3;
+    image.style.marginLeft = marginLeft + 'px';
+}
+
+image.onclick = function () {
+    var interval = setInterval(moveRight, 10);
+};
