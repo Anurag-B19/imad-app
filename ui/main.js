3a3d91ba260235ad9717b9e49756e7a0a9e80ac9
Anurag Bhattacharya <anurag.pro.01@gmail.com>
2017-08-17 21:14:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,13 +4,22 @@ var button = document.getElementById("counter");
 var counter =0;
 button.onclick = function () {
   
-  //Make a request to the counter endpoint
-  
+  //Create a request variable
+    var request = new XMLHttpRequest();
+    
   //Capture the response and store it in a variable
-  
-  //Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
-   
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                //Take some action
+                counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+  //Make the request
+   request.open('GET', "http://anuraged6.imad.hasura-app.io/counter", true);
+   request.send(null);
 };
