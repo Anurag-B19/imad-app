a5dd10fdbb405654ea29243e10b530e68c6517f0
Anurag Bhattacharya <anurag.pro.01@gmail.com>
2017-08-17 20:20:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 //Counter increment code
 
-var button = documnet.getElementById('counter');
+var button = documnet.getElementById("counter");
 var counter =0;
 button.onclick = function () {
   
@@ -10,7 +10,7 @@ button.onclick = function () {
   
   //Render the variable in the correct span
     counter = counter + 1;
-    var span = document.getElementById('count');
+    var span = document.getElementById("count");
     span.innerHTML = counter.toString();
   
 };
